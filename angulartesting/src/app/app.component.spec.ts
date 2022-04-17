e5f6e83d627940b5ee50d26fdd8144b2359f2bb0
AshutoshChoubey <38152014+AshutoshChoubey@users.noreply.github.com>
2022-04-17 23:26:47 +0530

    Updated app.spec.ts

--- angulartesting/src/app/app.component.spec.ts
+++ angulartesting/src/app/app.component.spec.ts
@@ -2,30 +2,41 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  beforeEach(() => {
+    var testingConfig = TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    })
+
+    testingConfig.compileComponents()
+  }
+  );
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const app = TestBed.createComponent(AppComponent).componentInstance
+    expect(app).toBeTruthy()
   });
 
   it(`should have as title 'angulartesting'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angulartesting');
+    const app = TestBed.createComponent(AppComponent).componentInstance
+    expect(app.title).toBe('angulartesting')
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angulartesting app is running!');
+    expect(fixture.nativeElement.querySelector("h1").textContent).toContain('angulartesting')
   });
+  it('should render changed title', () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges();
+    fixture.componentInstance.title = "typescript"
+    fixture.detectChanges();
+    console.log("🚀 ~ file: app.component.spec.ts ~ line 32 ~ it ~ fixture", fixture.componentInstance.m1(1,2,3))
+    expect(fixture.componentInstance.m1(1,2,3)).toBe(6)
+  });
+
+  afterEach(() => {
+    console.log('afterEach')
+  })
+
 });
