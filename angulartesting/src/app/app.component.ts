c65c38f9df577a63a501b27890fd0d141b10ca5b
AshutoshChoubey <38152014+AshutoshChoubey@users.noreply.github.com>
2022-04-30 16:22:06 +0530

    Update app.component.ts

--- angulartesting/src/app/app.component.ts
+++ angulartesting/src/app/app.component.ts
@@ -1,39 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { GenericService } from './generic.service';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'angulartesting';
-  // initForm1: FormGroup=new FormGroup({
-  //   user: new FormControl("",[Validators.required,Validators.pattern("/^[a-z]{1}[a-z0-9_]{3,13}$/")]),
-  //   pass: new FormControl("",[Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$")])
-  // });
-  data: { status: number; data: any; };
-  initForm: FormGroup=this.fb.group({
-    user: ["",[Validators.required,Validators.pattern("/^[a-z]{1}[a-z0-9_]{3,13}$/")]],
-    pass: ["",[Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$")]]
-  });
-  constructor(private g:GenericService, private fb: FormBuilder,){
-    
-  }
-  
+  passwithinput = { learn: 'angular', website: 'worldgyan.com' };
+  initForm: FormGroup;
+  data: { status: number; data: any };
+  constructor(
+    private fb: FormBuilder,
+    private genericService: GenericService,
+    private api: ApiService
+  ) {}
   ngOnInit(): void {
+    // this.initForm= new FormGroup({
+    //   user:new FormControl("",[Validators.required,Validators.pattern("^[a-zA-Z0-9-_]{3,13}$")]),
+    //   pass:new FormControl("",[Validators.required,Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&].{4,15})$")])
+    // })
+    this.initForm = this.fb.group({
+      user: [
+        '',
+        [Validators.required, Validators.pattern('^[a-z]{1}[a-z0-9_]{3,13}$')],
+      ],
+      pass: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
+          ),
+        ],
+      ],
+    });
   }
-  submit(){
-    this.g.postData(this.initForm.value).subscribe((val)=>{
-      this.data=val;
-      console.log("🚀 ~ file: app.component.ts ~ line 28 ~ AppComponent ~ this.g.postData ~ this.data", this.data)
-    })
-    
+  submit() {
+    if (this.initForm.valid) {
+      let user = this.initForm.get('user').value;
+      let pass = this.initForm.get('pass').value;
+      this.api.postData(user, pass).subscribe((val) => {
+        this.data = val;
+      });
+    }
   }
- 
-  sum(a,b,c){
-    return a+b+c;
-  }
- 
 }
